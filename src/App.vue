6d37a12e4591f30225c5864917cbe9e39a0b67a1
nguyentamthanh <thanhh16012k@gmail.com>
2022-11-25 16:22:19 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
       <img src="./assets/pinia-logo.svg" alt="" />
       <h1>{{ taskStore.name }}</h1>
     </header>
+    <!-- new task form -->
+    <div class="new-task-form">
+      <task-form />
+    </div>
     <!-- filter -->
     <nav class="filter">
       <button @click="filter = 'all'">all task</button>
@@ -12,14 +16,13 @@
     </nav>
     <!-- task list -->
     <div class="task-list" v-if="filter === 'all'">
-      <p>Your have {{ taskStore }}</p>
-      <p>all tasks</p>
+      <p>Your have {{ taskStore.totalCount }} tasks left to do</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
         <task-details :task="task" />
       </div>
     </div>
     <div class="task-list" v-if="filter === 'favs'">
-      <p>fav tasks</p>
+      <p>Your have {{ taskStore.favCount }} favs to do</p>
       <div v-for="task in taskStore.favs" :key="task.id">
         <task-details :task="task" />
       </div>
@@ -31,8 +34,9 @@
 import { ref } from "vue";
 import TaskDetails from "./components/TaskDetails.vue";
 import { useTaskStore } from "./stores/TaskStore";
+import TaskForm from "./components/TaskForm.vue";
 export default {
-  components: { TaskDetails },
+  components: { TaskDetails, TaskForm },
   setup() {
     const taskStore = useTaskStore();
     const filter = ref("all");
--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -2,15 +2,26 @@
   <div class="task">
     <h3>{{ task.title }}</h3>
     <div class="icons">
-      <i class="material-icons">delete</i>
-      <i class="material-icons">favorite</i>
+      <div>{{ taskScore }}</div>
+      <i class="material-icons" @click="taskStore.deleteTask(task.id)"
+        >delete</i
+      >
+      <i class="material-icons" @click="taskScore.toggleFav(task.id)"
+        >favorite</i
+      >
     </div>
   </div>
 </template>
 
 <script>
+import { useTaskStore } from "../stores/TaskStore";
+
 export default {
   props: ["task"],
+  setup() {
+    const taskScore = useTaskStore();
+    return { taskScore };
+  },
 };
 </script>
 
